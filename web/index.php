853704cd105b8f772349ac5311659620c0e029ca
jack <jawsjack3@gmail.com>
2015-04-06 10:35:27 -0700

    Commit message

---
+++ web/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Test page Daycare</title>
+        <meta name="description" content="">
+        <!-- Mobile viewport optimized: j.mp/bplateviewport -->
+        <meta name="viewport" content="width=device-width">
+    </head>
+    <body>
+	<?php echo "Hello! We are in the site DAYCARE!"; ?>
+	<?php phpinfo();?>
+	<body>
+</html>
